e1f85914c823382cf9cf9c990c9b802c8caddddf
Albert Queiroz <queiroz.albert@gmail.com>
2023-07-11 17:19:57 -0300

    Delete an unused class

--- src/main/java/br/com/albert/config/BotConfigStrings.java
+++
@@ -1,8 +0,0 @@
-package br.com.albert.config;
-
-public class BotConfigStrings {
-
-	public static final String BOT_USERNAME = "TestAlbertv10Bot";
-	public static final String BOT_TOKEN = "6053541303:AAHR752s5zdoyFVKELuixY01QCOadeZNSvg";
-	
-}
